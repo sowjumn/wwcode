fbd840ff1023326b04070bd089eb09fea4a93c8f
sowjanya <sowju19@gmail.com>
2014-02-23 19:29:31 -0800

    Added the locations controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Wwcode::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  resources :locations
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
