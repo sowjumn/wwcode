7729444f208e81e522ea89320508c24dd7ec731b
sowjanya <sowju19@gmail.com>
2014-03-11 00:14:48 -0700

    added blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -2,5 +2,22 @@ class BlogsController < ApplicationController
   before_action :signed_in_admin, only: [:new,:edit,:update]
 
   def new
+    @blog = Blog.new
   end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to current_admin
+    else
+      render 'new'
+    end
+  end
+
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title,:para1,:para2,:para3)
+    end
+
 end
