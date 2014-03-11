72fa6a7e2635942e23820f6280b9a9655f563885
sowjanya <sowju19@gmail.com>
2014-03-10 20:19:02 -0700

    more helper methods

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,4 +5,36 @@ module SessionsHelper
     admin.update_attribute(:remember_token,Admin.encrypt(remember_token))
     self.current_admin = admin
   end
+
+  def current_admin=(admin)
+    @current_admin = admin
+  end
+
+  def current_admin
+    remember_token = Admin.encrypt(cookies[:remember_token])
+    @current_admin ||= Admin.where(remember_token: remember_token).first
+  end
+
+  def signed_in?
+    !current_admin.nil?
+  end
+
+  def sign_out
+    current_admin.update_attribute(:remember_token, Admin.encrypt(Admin.new_remember_token))
+    cookies.delete(:remember_token)
+    self.current_admin = nil
+  end
+
+  def current_admin?(admin)
+    admin == current_admin
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
 end
